5d8913020688690eb37f9bdd39ae78fb6551d526
ale.settimi <ale.settimi@gmail.com>
2017-10-17 09:44:52 +0200

    updated example scripts

--- scripts/compile_example_unix.sh
+++ scripts/compile_example_unix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo -e "\e[1m[\e[32mcompiling all examples...\e[39m]\e[21m"
 
-echo -e "\e[1m[\e[33m - example\e[39m]\e[21m"
+echo -e "\e[1m[\e[33m - qbdevice_handler\e[39m]\e[21m"
 cd ../example/qbdevice_handler
 mkdir build
-g++ qbdevice_handler.cpp ../../qbAPI/src/qbmove_communications.cpp -o build/qbdevice
-cd ../../../scripts
+g++ -std=c++11 qbdevice_handler.cpp ../../qbAPI/src/qbmove_communications.cpp -o build/qbdevice_handler
+cd ../../scripts
